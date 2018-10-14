27a0d755647a378511a07de32ea0b192d971651f
Martin 王志俊 <martin@mymba.local>
2018-10-14 22:30:12 +0800

    Initial Commit

---
+++ socktest1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  socktest1
+//
+//  Created by wang zhijun on 2018/10/14.
+//  Copyright © 2018 Fendalton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
